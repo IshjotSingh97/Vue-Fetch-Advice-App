b6995c9673dca1192ebd74603398e7d322ec41cf
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2020-12-21 14:49:43 +0530

    api added

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Script Started")
+
+const API = 'https://api.adviceslip.com/advice'
+
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        heading: "Fetch Advice App Using Vue",
+        id: null,
+        advice : null
+    },
+    methods: {
+        async fetchdata() {
+            axios.get(API)
+                .then((response) => {
+                    this.id = response.data.slip.id
+                    this.advice = response.data.slip.advice
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    },
+    mounted(){
+    	this.fetchdata()
+    }
+
+})
